05f367f63d30af99384eb59ec0a4e87ce1b62e78
JinhyeonYu <williamjinhyeon1@gmail.com>
2023-11-22 16:37:54 +0900

    Created  Heart

--- gamemath-3-1/gamemath-3-1/Source/Player/SoftRenderer2D.cpp
+++ gamemath-3-1/gamemath-3-1/Source/Player/SoftRenderer2D.cpp
@@ -53,7 +53,8 @@ void SoftRenderer::LoadScene2D()
 }
 
 // 게임 로직과 렌더링 로직이 공유하는 변수
-Vector2 currentPosition(100.f, 100.f);
+Vector2 currentPosition;
+float currentScale = 10.f;
 
 // 게임 로직을 담당하는 함수
 void SoftRenderer::Update2D(float InDeltaSeconds)
@@ -62,11 +63,16 @@ void SoftRenderer::Update2D(float InDeltaSeconds)
 	auto& g = Get2DGameEngine();
 	const InputManager& input = g.GetInputManager();
 
-	// 게임 로직의 로컬 변수
-	const float moveSpeed = 100.f;
-	Vector2 inputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis));
+	static float moveSpeed = 100.f;
+	static float scaleMin = 5.f;
+	static float scaleMax = 20.f;
+	static float scaleSpeed = 20.f;
+	
+	Vector2 inputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis)).GetNormalize();
 	Vector2 deltaPosition = inputVector * moveSpeed * InDeltaSeconds;
+	float deltaScale = input.GetAxis(InputAxis::ZAxis) * scaleSpeed * InDeltaSeconds;
 	currentPosition += deltaPosition;
+	currentScale = Math::Clamp(currentScale + deltaScale, scaleMin, scaleMax);
 }
 
 
@@ -80,31 +86,33 @@ void SoftRenderer::Render2D()
 
 	// 배경에 격자 그리기
 	DrawGizmo2D();
+	
+	float rad = 0.f;
+	static float increment = 0.001f;
+	static std::vector<Vector2> hearts;
 
-	// 렌더링 로직의 로컬 변수
-	const float radius = 50.f;
-	static std::vector<Vector2> circles;
-	if (circles.empty())
+	if (hearts.empty())
 	{
-		++circleFullfill_Time;
-		for (float x = -radius; x <= radius; ++x)
+		for (rad = 0.f; rad < Math::TwoPI; rad += increment)
 		{
-			for (float y = -radius; y <= radius; ++y)
-			{
-				Vector2 pointToTest = Vector2(x, y);
-				float squaredLength = pointToTest.SizeSquared();
-
-				if (squaredLength <= radius * radius)
-					circles.push_back(Vector2(x, y));
-			}
+			float sin = sinf(rad);
+			float cos = cosf(rad);
+			float cos2 = cosf(2 * rad);
+			float cos3 = cosf(3 * rad);
+			float cos4 = cosf(4 * rad);
+			float x = 16.f * sin * sin * sin;
+			float y = (13 * cos) - (5 * cos2) - (2 * cos3) - cos4;
+			hearts.push_back(Vector2(x, y));
 		}
 	}
 
-	for (auto const& v : circles)
-		r.DrawPoint(v + currentPosition, LinearColor::Red);
+	for (auto const& v : hearts)
+	{
+		r.DrawPoint(v * currentScale + currentPosition, LinearColor::Blue);
+	}
 
-    r.PushStatisticText("Cricle Played Time = " + std::to_string(circleFullfill_Time));
-	r.PushStatisticText("Coordinate : " + currentPosition.ToString());
+	r.PushStatisticText(std::string("Position : ") + currentPosition.ToString());
+	r.PushStatisticText(std::string("Scale : ") + std::to_string(currentScale));
 }
 
 // 메시를 그리는 함수
